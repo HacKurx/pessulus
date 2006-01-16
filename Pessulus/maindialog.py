fa80ae14aa0efb573e394aeee5bf3c36920abb9a
Vincent Untz <vuntz@gnome.org>
2006-01-16 21:18:49 +0000

    s/_Fullscreen/Force _fullscreen mode/, add some keys for gnome-screensaver
    2006-01-16  Vincent Untz  <vuntz@gnome.org>
    
    	* Pessulus/maindialog.py: s/_Fullscreen/Force _fullscreen mode/,
    	add some keys for gnome-screensaver
    	* pessulus.glade: add a tab for gnome-screensaver

--- Pessulus/maindialog.py
+++ Pessulus/maindialog.py
@@ -60,8 +60,11 @@ lockdownbuttons = (
     ( "/apps/epiphany/lockdown/disable_history", _("Disable _history"), "vbox9" ),
     ( "/apps/epiphany/lockdown/disable_javascript_chrome", _("Disable _javascript chrome"), "vbox9" ),
     ( "/apps/epiphany/lockdown/disable_toolbar_editing", _("Disable _toolbar editing"), "vbox9" ),
-    ( "/apps/epiphany/lockdown/fullscreen", _("_Fullscreen"), "vbox9" ),
-    ( "/apps/epiphany/lockdown/hide_menubar", _("Hide _menubar"), "vbox9" )
+    ( "/apps/epiphany/lockdown/fullscreen", _("Force _fullscreen mode"), "vbox9" ),
+    ( "/apps/epiphany/lockdown/hide_menubar", _("Hide _menubar"), "vbox9" ),
+    ( "/apps/gnome-screensaver/lock_enabled", _("_Lock on activation"), "vbox15" ),
+    ( "/apps/gnome-screensaver/logout_enabled", _("Allow log _out"), "vbox15" ),
+    ( "/apps/gnome-screensaver/user_switch_enabled", _("Allow user _switching"), "vbox15" )
 )
 
 class PessulusMainDialog:
