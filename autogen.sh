65cc8e33a3261b19d14fc3635f0961a6f5845c84
Vincent Untz <vuntz@gnome.org>
2010-09-08 19:24:21 +0200

    [misc] Update HACKING, MAINTAINERS, README
    Nothing new, but make sure this is up-to-date.
    
    Also tweak a bit autogen.sh and Makefile.am to make them look similar in
    all my modules.

--- autogen.sh
+++ autogen.sh
@@ -6,9 +6,10 @@ test -z "$srcdir" && srcdir=.
 
 PKG_NAME="pessulus"
 REQUIRED_AUTOMAKE_VERSION=1.9
+REQUIRED_M4MACROS=
 
 (test -f $srcdir/configure.ac \
-  && test -d $srcdir/Pessulus) || {
+  && test -f $srcdir/$PKG_NAME.doap) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
     echo " top-level $PKG_NAME directory"
     exit 1
@@ -18,7 +19,5 @@ which gnome-autogen.sh || {
     echo "You need to install gnome-common."
     exit 1
 }
-
-test -d $srcdir/m4 || mkdir $srcdir/m4
 
 . gnome-autogen.sh
